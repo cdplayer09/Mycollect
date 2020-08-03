882c7409410d8a84f7812746bd420f678cec435f
cdplayer09 <ciarandepree123hotmail.comgit config --global user.email ciarandepree123@hotmail.comgit config --global user.email ciarandepree123hotmail.com>
2020-08-03 08:34:25 -0700

    final commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,26 @@ require("channels")
 
 require("trix")
 require("@rails/actiontext")
+
+import '../stylesheets/application';
+//listner for paragraph content then adds class "d-none" and removes class "dnone" from edit page allowing a button to cancel.
+document.addEventListener('turbolinks:load', () => {
+    document.addEventListener('click', () => {
+      let element = event.target.closest('.paragraph-content')
+      if (!element) return;
+  
+      element.classList.add('d-none')
+      element.nextElementSibling.classList.remove('d-none')
+    })
+  
+    document.addEventListener('click', () => {
+      if (!event.target.matches('.cancel')) return;
+  
+      event.preventDefault();
+  
+      let element = event.target.closest('.paragraph-form')
+  
+      element.classList.add('d-none')
+      element.previousElementSibling.classList.remove('d-none')
+    })
+  })
